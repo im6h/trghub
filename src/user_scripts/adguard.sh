7a28a943610e4f76a7e5172450f43d908bf543a7
im6h <danghaiv.18@gmail.com>
2023-09-09 16:20:12 +0700

    chore: update script to install docker and pass docker containe

--- src/user_scripts/adguard.sh
+++ src/user_scripts/adguard.sh
@@ -1,6 +1,35 @@
 #!/bin/sh
 
+# update new package
 sudo apt update
 sudo apt install snapd -y
 
-sudo snap install adguard-home
+# change dns port
+sudo mkdir -p /etc/systemd/resolved.conf.d
+sudo tee /etc/systemd/resolved.conf.d/adguardhome.conf <<EOF
+[Resolve]
+DNS=127.0.0.1
+DNSStubListener=no
+EOF
+
+sudo mv /etc/resolv.conf /etc/resolv.conf.backup
+sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+sudo systemctl reload-or-restart systemd-resolved
+
+# install newrelic
+curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY=NRAK-VQ6UP8JW5E66UZHV1AVC7CN735M NEW_RELIC_ACCOUNT_ID=4087408 /usr/local/bin/newrelic install -y
+
+# install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# install adguardhome via dockere
+sudo mkdir adguardhome
+cd adguardhome
+docker run --name adguardhome\
+    --restart unless-stopped\
+    -v ./:/opt/adguardhome/work\
+    -v ./:/opt/adguardhome/conf\
+    -p 53:53/tcp -p 53:53/udp\
+    -p 80:80/tcp -p 443:443/tcp -p 443:443/udp -p 3000:3000/tcp\
+    -d adguard/adguardhome
